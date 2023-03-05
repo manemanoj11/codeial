cdade8af01c92010ffefcdcab1022cb2662de01d
Mane <manemanoj850@gmail.com>
2023-03-05 18:35:02 +0530

    Initail commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import ReactDOM from "react-dom/client";
+import React from 'react';
+// import ReactDOM from 'react-dom';
+import './styles/index.css';
+import { App } from './components';
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<App />);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
